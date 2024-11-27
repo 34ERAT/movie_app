5da38d940d1031aee846464a7e9f29256414b30b
34ERAT <philipkanuti@gmail.com>
2024-11-27 12:47:30 +0300

    adding features to the sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,7 @@
+import { Hidden, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const navLinks = [
   {
     name: "Home",
@@ -46,7 +47,69 @@ const Sidebar = () => {
       }}
     >
       <Box
-      ></Box>
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "row",
+            lg: "column"
+          },
+          gap: 5,
+          alignItems: {
+            xs: "center",
+            ls: "start"
+          },
+          width: "100%"
+        }}
+      >
+        <Hidden smDown>
+          <Typography variant="h5" component="h1" my={2} fontWeight={400} fontSize={18} >Movie app</Typography>
+        </Hidden>
+        <Box sx={{
+          py: {
+            xs: "0px",
+            ls: "16px"
+          },
+          display: "flex",
+          flexDirection: {
+            xs: "row",
+            lg: "column"
+          },
+          gap: 4
+
+        }
+        }>
+          {navLinks.map((item) => (
+            <Link key={item.name} to={item.link} style={{ textDecoration: "none" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 2,
+                  color: "white",
+                  textDecoration: "none"
+                }}
+              >
+                <img src={item.icon} alt={item.name}
+                  style={{
+                    width: "18px",
+                    filter: `${pathname === item.link ?
+                      " invert(58%) sepia(14) saturate(3166) hue-rotate(215) brightness()"
+                      :
+                      "invert(84%)"
+                      }`
+                  }}
+                />
+                <Hidden mdDown>
+                  <Typography> {item.name}</Typography>
+                </Hidden>
+              </Box>
+            </Link>
+
+          ))
+          }
+
+        </Box>
+      </Box>
     </Box>
   );
 };
