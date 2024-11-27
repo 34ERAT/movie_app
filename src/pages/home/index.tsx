a129e9065470f5faab63ddd0f63d7a761a5498c5
34ERAT <philipkanuti@gmail.com>
2024-11-27 12:48:50 +0300

    add  search input

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,28 @@
-
+import { Box, InputBase, Paper, TextField } from "@mui/material";
 import React from "react";
 import Layout from "../../Layout";
 const Home = () => {
   return (
     <Layout>
-      Home
+      <Box>
+        <Paper
+          component="form"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "default",
+            p: 1,
+            backgroundColor: "#10141f",
+            border: "none",
+          }}
+        >
+          <InputBase
+            placeholder="search for movies or tv series"
+            // inputProps={{ "aria-label": "enter text" }}
+            sx={{ width: "100%", }}
+          ></InputBase>
+        </Paper>
+      </Box>
     </Layout>
   );
 };
