a1926a47e8a7c8480307fae3a5b81859a0307390
34ERAT <philipkanuti@gmail.com>
2024-11-29 10:23:54 +0300

    movies list components

---
+++ src/components/movie-list/movieTrendList.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+interface MovieTrendListProps {
+  TrendList: any;
+}
+const MovieTrendList = ({ trendList }: any) => {
+  return (
+    <div>
+      MovieTrendList
+    </div>
+  )
+}
+
+export default MovieTrendList;
